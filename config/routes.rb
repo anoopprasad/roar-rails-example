0f1763f7174d59c9180e8e5307752960a2b37d60
Gary S. Weaver <garysweaver@gmail.com>
2012-09-17 13:38:18 -0400

    allowing puts without id specified that route to create like a post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RoarRailsExample::Application.routes.draw do
   get "home/index"
   root :to => 'home#index'
-  resources :employees #, :constraints => {:format => /json/}, :defaults => { :format => 'json' }
-  resources :companies #, :constraints => {:format => /json/}, :defaults => { :format => 'json' }
+  resources :employees
+  match 'employees(.:format)' => 'employees#create', :via => :put # allow putting without an id to act as post
+  resources :companies
+  match 'companies(.:format)' => 'companies#create', :via => :put # allow putting without an id to act as post
 end
